edf29022893021a9be64303a0dc1255273e87c59
Sinister_30N4 <36511097+tongJK@users.noreply.github.com>
2019-06-11 11:33:15 +0700

    Add files via upload

---
+++ keras_train_model.py
@@ -0,0 +1,56 @@
+
+import os
+import keras
+from keras.callbacks import ReduceLROnPlateau
+from keras.preprocessing.image import ImageDataGenerator
+from keras.callbacks import CSVLogger, ModelCheckpoint, EarlyStopping
+
+from resnet_152 import resnet152_model
+
+verbose = 1
+patience = 50
+batch_size = 16
+num_channels = 3
+img_width, img_height = 224, 224
+
+train_data = 'train'
+validation_data = 'validation'
+
+num_classes = 224
+num_train_samples = 9985
+num_valid_samples = 2126
+
+num_epochs = 100
+
+if __name__ == '__main__':
+    # build a classifier model
+    model = resnet152_model(img_height, img_width, num_channels, num_classes)
+
+    # prepare data augmentation configuration
+    train_data_gen = ImageDataGenerator(rotation_range=20., width_shift_range=0.1, height_shift_range=0.1,
+                                        zoom_range=0.2, rescale=1. / 255, horizontal_flip=True)
+    valid_data_gen = ImageDataGenerator()
+
+    # callbacks
+    tensor_board = keras.callbacks.TensorBoard(log_dir='./logs', histogram_freq=0, write_graph=True, write_images=True)
+    log_file_path = 'logs/training.log'
+    csv_logger = CSVLogger(log_file_path, append=False)
+    early_stop = EarlyStopping('val_acc', patience=patience)
+    reduce_lr = ReduceLROnPlateau('val_acc', factor=0.1, patience=int(patience / 4), verbose=1)
+
+    trained_models_path = 'models/'
+    model_names = os.path.join(trained_models_path, 'model.{val_acc:.2f}_at_{epoch:02d}.hdf5')
+
+    model_checkpoint = ModelCheckpoint(model_names, monitor='val_acc', verbose=1, save_best_only=True)
+    callbacks = [tensor_board, model_checkpoint, csv_logger, early_stop, reduce_lr]
+
+    # generators
+    train_generator = train_data_gen.flow_from_directory(train_data, (img_width, img_height), batch_size=batch_size,
+                                                         class_mode='categorical')
+    valid_generator = valid_data_gen.flow_from_directory(validation_data, (img_width, img_height),
+                                                         batch_size=batch_size, class_mode='categorical')
+
+    # fine tune the model
+    model.fit_generator(train_generator, steps_per_epoch=num_train_samples / batch_size,
+                        validation_data=valid_generator, validation_steps=num_valid_samples / batch_size,
+                        epochs=num_epochs, callbacks=callbacks, verbose=verbose)
